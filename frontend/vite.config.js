905859600f0435002d2ff8962e3d4643137fb17c
vserkankrc <@://github.com/vserkankrc/e-ticaret.git>
2025-08-01 23:11:45 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'  // Bunu ekle
+import path from 'path' 
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',  // Burası önemli
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
-      '@': path.resolve(__dirname, 'src'),  // '@' alias'ını tanımla
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   optimizeDeps: {
