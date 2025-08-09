6cef44cd9171922fab49b25d646f0006199832ed
vserkankrc <@://github.com/vserkankrc/e-ticaret.git>
2025-08-09 22:47:08 +0300

    Update auth.js

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -99,7 +99,7 @@ router.post("/login", async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
-
+        domain: '.tercihsepetim.com',
         path: "/",
         maxAge: 60 * 60 * 1000, // 1 saat
       })
@@ -157,6 +157,7 @@ router.post("/logout", (req, res) => {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    domain: '.tercihsepetim.com',
     path: "/",
   });
   res.status(200).json({ message: "Çıkış başarılı." });
